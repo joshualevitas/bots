69dce7b7df7c2c0a4220eb5d1a9a61cd9ba2caa5
Josh <61065503+joshualevitas@users.noreply.github.com>
2023-02-06 14:48:16 -0600

    scorpion

--- constants.py
+++ constants.py
@@ -4,13 +4,10 @@ import numpy
 num_steps = 1000 
 max_force = 500 
 sleep_time = 0
-
 amplitude_front = numpy.pi/4 
 frequency_front = 50
 phaseOffset_front = 0 
 targetAngles_front = numpy.linspace(0 , 2*numpy.pi, num = 1000)
-
-
 amplitude_back = numpy.pi/4 
 frequency_back = 50
 phaseOffset_back = numpy.pi/8
@@ -19,9 +16,7 @@ targetAngles_back = numpy.linspace(0 , 2*numpy.pi, num = 1000)
 
 numberOfGenerations = 10
 populationSize = 10
-
-
-numSensorNeurons = 12 #8 
-numMotorNeurons = 11 # 7 
-
 motorJointRange = 0.5
+
+numSensorNeurons = 16 
+numMotorNeurons = 14 
